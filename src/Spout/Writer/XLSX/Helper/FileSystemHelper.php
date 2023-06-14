fed44b84f7630221c517d40e8799989cb47d9f4b
Ahmad Ghuneim <ahmad.ghuneim@hyperpay.com>
2023-06-14 16:11:08 +0300

    changedTimeZoneAttrInDateTime

--- src/Spout/Writer/XLSX/Helper/FileSystemHelper.php
+++ src/Spout/Writer/XLSX/Helper/FileSystemHelper.php
@@ -200,7 +200,7 @@ EOD;
      */
     private function createCoreXmlFile()
     {
-        $createdDate = (new \DateTime())->format(\DateTime::W3C);
+        $createdDate = (new \DateTime())->format("Y-m-d\TH:i:sp");
         $coreXmlFileContents = <<<EOD
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <cp:coreProperties xmlns:cp="http://schemas.openxmlformats.org/package/2006/metadata/core-properties" xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:dcmitype="http://purl.org/dc/dcmitype/" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
